bf19886165d98b25e27f5fbd462696c5e0955c16
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-08-17 00:35:01 -0300

    add auth controller

---
+++ src/modules/users/controllers/AuthController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import LoginService from '../services/LoginService';
+
+export default class AuthController {
+  async login(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticate = new LoginService();
+
+    const user = authenticate.execute({ email, password });
+
+    return response.json(user);
+  }
+}
