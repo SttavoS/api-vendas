ce2a0fb343761e735518ab97cf64d6ae7171608a
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-08-21 20:26:37 -0300

    create a static router to share the user avatar file path

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,13 +6,14 @@ import { errors } from 'celebrate';
 import routes from './routes';
 import { AppError } from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 const port = 3333;
-
+// TODO: Refarorar, separando em um arquivo app.ts
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
