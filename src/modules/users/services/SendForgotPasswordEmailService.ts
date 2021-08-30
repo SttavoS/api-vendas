faf00875d2107f66b01b7cbe043302471af06557
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-08-30 17:22:30 -0300

    refactor(services): change to use handlebars views instead of inline template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 import { AppError } from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
+import path from 'path';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
@@ -19,7 +20,14 @@ export default class SendForgotPasswordEmailService {
       throw new AppError('User does not exits.', 404);
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot-password.hbs',
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -28,10 +36,10 @@ export default class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de senha',
       templateData: {
-        template: `Solicitação de redefinição de senha recebida: ${token.token}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token: token.token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
