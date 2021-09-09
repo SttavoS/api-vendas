087686b4864578d05c5c0c0c0d47d0507fdf434a
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-09-09 16:07:39 -0300

    feat(users): add profile routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import authRouter from '@modules/users/routes/auth.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/auth', authRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
