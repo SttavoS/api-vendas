8426d1a82e26ae8a79b969fb06270fcf2c09fd6c
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-09-10 21:27:35 -0300

    feat(customers): add a service to show one customer

---
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -0,0 +1,18 @@
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+export default class ShowCustomerService {
+  async execute(id: string): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+
+    const customer = await customersRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found', 404);
+    }
+
+    return customer;
+  }
+}
