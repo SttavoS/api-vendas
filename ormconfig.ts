f89d2f4748f4501b25c1f702d81648c0b5cf337d
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-10-01 16:39:54 -0300

    fix: change the extension of ormconfig to .ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,13 @@
-module.exports = {
+export default {
   type: 'postgres',
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
-  migrations: [process.env.TYPEORM_MIGRATIONS],
+  entities: ['./src/modules/**/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
   cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+    migrationsDir: './src/shared/typeorm/migrations/',
   },
 };
