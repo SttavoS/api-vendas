07025628dc6c25624ca3f952a279a62bccda67a8
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2021-10-08 16:30:30 -0300

    feat(orders): add orders_products entity

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,15 +1,20 @@
+import OrdersProducts from '@modules/orders/typeorm/entities/OrdersProducts';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('products')
-export class Product {
+export default class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
+  order_products: OrdersProducts[];
 
   @Column()
   name: string;
